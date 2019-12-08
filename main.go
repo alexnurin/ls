326af6bef8697c758910efe1fd484864c4b88fc5
Нарин Алексей <an362@mail.ru>
2019-12-08 11:10:01 +0300

    Formatted file size (flag -h)

--- main.go
+++ main.go
@@ -5,22 +5,27 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strconv"
 )
 
 var (
 	d = flag.String("d", ".", "Directory to process")
 	a = flag.Bool("a", false, "Print all info")
+	h = flag.Bool("h", false, "Print all info")
 )
 
 func hrSize(fsize int64) string {
-	return "0KB"
+	szs := [5]string{"B", "KB", "MB", "GB", "TB"}
+	id := 0
+	for fsize > 1024 {
+		id++
+		fsize = fsize / 1024
+	}
+	return fmt.Sprintf("%d%s", fsize, szs[id])
 }
 
 func printAll(file os.FileInfo) {
-	time := file.ModTime().Format("Jan 06 15:4")
-	fSize := strconv.Itoa(int(file.Size()))
-	fmt.Printf("%s %s %s \n", fSize, time, file.Name())
+	time := file.ModTime().Format("Jan 01 15:4")
+	fmt.Printf("%s %s %s \n", hrSize(file.Size()), time, file.Name())
 }
 
 func main() {
