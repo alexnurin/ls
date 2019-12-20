a4b93d949e5f3289004b29d76fe0e5040cc6bddf
Нарин Алексей <an362@mail.ru>
2019-12-20 22:50:34 +0300

    Fixed sort, flags

--- main.go
+++ main.go
@@ -8,19 +8,33 @@ import (
 )
 
 var (
-	d = flag.String("d", ".", "Directory to process")
-	a = flag.Bool("a", false, "Print all info")
-	h = flag.Bool("h", false, "Print all info")
+	d    = flag.String("d", ".", "Directory to process")
+	a    = flag.Bool("a", false, "Print all info")
+	h    = flag.Bool("h", false, "Visual size info")
+	sort = flag.Bool("sort", false, "Sorted by date")
 )
 
-func hrSize(fsize int64) string {
-	szs := [5]string{"B", "KB", "MB", "GB", "TB"}
-	id := 0
-	for fsize > 1024 {
-		id++
-		fsize = fsize / 1024
+func bsort(arr *[]os.FileInfo) {
+	for i := 0; i < len(*arr); i++ {
+		for j := 0; j < len(*arr)-i-1; j++ {
+			if (*arr)[j].Size() > (*arr)[j+1].Size() {
+				(*arr)[j], (*arr)[j+1] = (*arr)[j+1], (*arr)[j]
+			}
+		}
 	}
-	return fmt.Sprintf("%d%s", fsize, szs[id])
+}
+func hrSize(fsize int64) string {
+	if *h {
+		szs := [6]string{"B", "KB", "MB", "GB", "TB", "PB"}
+		id := 0
+		for fsize >= 1024 {
+			id++
+			fsize = (fsize-1)/1024 + 1
+		}
+		return fmt.Sprintf("%d%s", fsize, szs[id])
+	} else {
+		return fmt.Sprintf("%d", fsize)
+	}
 }
 
 func printAll(file os.FileInfo) {
@@ -31,7 +45,9 @@ func printAll(file os.FileInfo) {
 func main() {
 	flag.Parse()
 	files, _ := ioutil.ReadDir(*d)
-
+	if *sort {
+		bsort(&files)
+	}
 	for _, file := range files {
 		if *a {
 			printAll(file)
